5b5342ef8047c9f760acb8ee32fbf6f1ccafb497
sergiosanchez71 <sergiosanalv@gmail.com>
2022-05-22 20:39:03 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,21 @@
-hola
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Trivial Quiz</title>
+</head>
+<body>
+
+	<h1>Bienvenido a Trivial Quiz</h1>
+	<form>
+	<p>¿A qué cuestionario te gustaría enfrentarte?</p>
+	<select>
+		<option>Cuestionario 1</option>
+	</select>
+	<button>Jugar</button>	
+	</form>
+
+
+</body>
+</html>
