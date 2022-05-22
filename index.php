561cb07f0e3e5bffb83435201344205652356835
sergiosanchez71 <sergiosanalv@gmail.com>
2022-05-22 21:15:31 +0200

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
 	<?php
 
-	require_once conexion.php;
+	include("conexion.php");
 
 	//$sql = "INSERT INTO questionnaires('1','Cuestionario 2','10',NULL)";
 	$questionnaires_sql = "SELECT * FROM questionnaires";
