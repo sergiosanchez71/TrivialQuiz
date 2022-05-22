4f0ca3c2d8fa7ff399dfd3ac2c84b23abab968d5
sergiosanchez71 <sergiosanalv@gmail.com>
2022-05-22 20:48:14 +0200

    Update index.php

--- index.php
+++ index.php
@@ -5,10 +5,13 @@
 
 	$mysqli = new mysqli("localhost", "u196455503_admin", "=e8Gl[oA6p", "u196455503_TrivialQuiz");
 
-if ($mysqli->connect_errno) {
-    echo "Fallo al conectar a MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
-}
-echo $mysqli->host_info . "\n";
+	if ($mysqli->connect_errno) {
+	    echo "Fallo al conectar a MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+	}
+	echo $mysqli->host_info . "\n";
+
+	$sql = 'SELECT id FROM questionnaires WHERE id = 1';
+	$resultado = mysql_query($sql, $enlace);
 
 	?>
 	<meta charset="utf-8">
@@ -21,7 +24,7 @@ echo $mysqli->host_info . "\n";
 	<form>
 	<p>¿A qué cuestionario te gustaría enfrentarte?</p>
 	<select>
-		<option>Cuestionario 1</option>
+		<option>Cuestionario 1 <?php echo $resultado ?></option>
 	</select>
 	<button>Jugar</button>	
 	</form>
