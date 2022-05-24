a31c4908c53f4a4c17f18a4406a4be02a8b5628d
sergiosanchez71 <sergiosanalv@gmail.com>
2022-05-24 19:34:46 +0200

    html template

--- index.php
+++ index.php
@@ -1,41 +1,70 @@
-<!DOCTYPE html>
+<!DOCTYPE HTML>
+<!--
+	Fractal by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
 <html>
 <head>
-	<?php
-
-	include("conexion.php");
-
-	//$sql = "INSERT INTO questionnaires('1','Cuestionario 2','10',NULL)";
-	$questionnaires_sql = "SELECT * FROM questionnaires";
-	$questionnaires = $mysqli->query($questionnaires_sql);
-
-
-	?>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Trivial Quiz</title>
+	<title>Fractal by HTML5 UP</title>
+	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+	<link rel="stylesheet" href="assets/css/main.css" />
+	<noscript><link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
 </head>
-<body>
+<body class="is-preload">
 
-	<h1>Bienvenido a Trivial Quiz</h1>
-	<form>
-	<p>¿A qué cuestionario te gustaría enfrentarte?</p>
-	<select>
-		<option>
-			<?php
+	<!-- Header -->
+	<header id="header">
+		<div class="content">
 
-				if ($questionnaires) {
-					foreach ($questionnaires as $questionary) {
-						echo $questionary['name'];
-					}
-				}
+			<h1>Bienvenido a Trivial Quiz</h1>
+			<form>
+				<p>¿A qué cuestionario te gustaría enfrentarte?</p>
+				<select>
+					<option>
+						<?php
 
-			?>
-		</option>
-	</select>
-	<button>Jugar</button>	
-	</form>
+							if ($questionnaires) {
+								foreach ($questionnaires as $questionary) {
+									echo $questionary['name'];
+								}
+							}
 
+						?>
+					</option>
+				</select>
+				<button>Jugar</button>	
+			</form>
+			
+			<h1><a href="#">Fractal</a></h1>
+			<p>Just a simple, single page responsive<br />
+				template brought to you by <a href="http://html5up.net">HTML5 UP</a></p>
+				<ul class="actions">
+					<li><a href="#" class="button primary icon solid fa-download">Download</a></li>
+					<li><a href="#one" class="button icon solid fa-chevron-down scrolly">Learn More</a></li>
+				</ul>
+			</div>
+			<div class="image phone"><div class="inner"><img src="images/screen.jpg" alt="" /></div></div>
+		</header>
 
-</body>
-</html>
+		<!-- Footer -->
+		<footer id="footer">
+			<ul class="icons">
+				<li><a href="#" class="icon brands fa-facebook-f"><span class="label">Facebook</span></a></li>
+				<li><a href="#" class="icon brands fa-twitter"><span class="label">Twitter</span></a></li>
+				<li><a href="#" class="icon brands fa-instagram"><span class="label">Instagram</span></a></li>
+			</ul>
+			<p class="copyright">&copy; Untitled. Credits: <a href="http://html5up.net">HTML5 UP</a></p>
+		</footer>
+
+		<!-- Scripts -->
+		<script src="assets/js/jquery.min.js"></script>
+		<script src="assets/js/jquery.scrolly.min.js"></script>
+		<script src="assets/js/browser.min.js"></script>
+		<script src="assets/js/breakpoints.min.js"></script>
+		<script src="assets/js/util.js"></script>
+		<script src="assets/js/main.js"></script>
+
+	</body>
+	</html>
