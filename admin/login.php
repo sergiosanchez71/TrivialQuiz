82d4c272249a6a94bac60e59833fca5a7fec03e2
sergiosanchez71 <sergiosanalv@gmail.com>
2022-05-24 20:52:35 +0200

    Update login.php

--- admin/login.php
+++ admin/login.php
@@ -82,15 +82,6 @@
 	<script src="../assets/js/breakpoints.min.js"></script>
 	<script src="../assets/js/util.js"></script>
 	<script src="../assets/js/main.js"></script>
-	<?php
-
-	
-	function login(){
-
-		echo "he entrado";
-	}
-
-	?>
 
 </body>
 </html>
