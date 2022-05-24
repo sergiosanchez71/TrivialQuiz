fdbadcbe3712fd8fadabc88079ad0ffff1c8c68a
sergiosanchez71 <sergiosanalv@gmail.com>
2022-05-24 21:19:53 +0200

    Update login.php

--- admin/login.php
+++ admin/login.php
@@ -1,6 +1,48 @@
 <!DOCTYPE HTML>
 <head>
 
+
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Trivial Quiz</title>
+	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+	<link rel="stylesheet" href="../assets/css/main.css" />
+	<noscript><link rel="stylesheet" href="../assets/css/noscript.css" /></noscript>
+
+	<style type="text/css">
+		a{
+			margin-top: 20px;
+			width: 100%;
+		}
+	</style>
+
+</head>
+<body class="is-preload">
+
+	<!-- Header -->
+	<header id="header">
+		<div class="content">
+
+			<div id="login">
+				<h1>Login admin</h1>
+				<form>
+					Usuario <input type="text" id="user">
+					Contraseña <input type="password" id="password">
+					<a id="login" class="button primary icon solid fa-comments-question">Loguearme</a>
+					<a href="../index.php" class="button icon solid fa-arrow-left">Volver</a>
+				</form>
+			</div>
+
+		</div>
+	</header>
+
+	<!-- Footer -->
+	<footer id="footer">
+		<p class="copyright">&copy; Sergio Sánchez Álvarez.</p>
+	</footer>
+
+
 	<!-- Scripts -->
 	<script src="../assets/js/jquery.min.js"></script>
 	<script src="../assets/js/jquery.scrolly.min.js"></script>
@@ -46,46 +88,6 @@
 				enter();
 		}
 	</script>
-
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Trivial Quiz</title>
-	<meta charset="utf-8" />
-	<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
-	<link rel="stylesheet" href="../assets/css/main.css" />
-	<noscript><link rel="stylesheet" href="../assets/css/noscript.css" /></noscript>
-
-	<style type="text/css">
-		a{
-			margin-top: 20px;
-			width: 100%;
-		}
-	</style>
-
-</head>
-<body class="is-preload">
-
-	<!-- Header -->
-	<header id="header">
-		<div class="content">
-
-			<div id="login">
-				<h1>Login admin</h1>
-				<form>
-					Usuario <input type="text" id="user">
-					Contraseña <input type="password" id="password">
-					<a id="login" class="button primary icon solid fa-comments-question">Loguearme</a>
-					<a href="../index.php" class="button icon solid fa-arrow-left">Volver</a>
-				</form>
-			</div>
-
-		</div>
-	</header>
-
-	<!-- Footer -->
-	<footer id="footer">
-		<p class="copyright">&copy; Sergio Sánchez Álvarez.</p>
-	</footer>
 
 </body>
 </html>
