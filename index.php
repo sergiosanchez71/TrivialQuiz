d6d4f33426d3592d7fd4ff055115ecd3f9b85938
sergiosanchez71 <sergiosanalv@gmail.com>
2022-05-24 19:41:03 +0200

    add quiz image

--- index.php
+++ index.php
@@ -44,7 +44,7 @@
 			</form>
 			
 		</div>
-		<div class="image phone"><div class="inner"><img src="images/screen.jpg" alt="" /></div></div>
+		<div class="image phone"><div class="inner"><img src="img/quiz.jpg" alt="" /></div></div>
 	</header>
 
 	<!-- Footer -->
