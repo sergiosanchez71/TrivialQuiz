db25cedadc873dab35b83f499c75c95e428e4c7b
sergiosanchez71 <sergiosanalv@gmail.com>
2022-05-27 16:48:13 +0200

    Update actions.php

--- admin/controller/actions.php
+++ admin/controller/actions.php
@@ -19,6 +19,7 @@ switch ($action) {
 		foreach ($users as $user) {
 			if ($username == $user['name']){
 				if (Password::verify($password,  $user['password'])) {
+					console.log('login true');
 					$_SESSION['admin'] = true;
 				} else {
 					echo "Contraseña incorrecta!";
