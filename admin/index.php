be630c4a553cb7a78adec8333206f07c1a3e0e34
sergiosanchez71 <sergiosanalv@gmail.com>
2022-06-08 19:27:16 +0200

    Update index.php

--- admin/index.php
+++ admin/index.php
@@ -98,6 +98,12 @@
 	</footer>
 
 	<!-- Scripts -->
+	<script src="../assets/js/jquery.min.js"></script>
+	<script src="../assets/js/jquery.scrolly.min.js"></script>
+	<script src="../assets/js/browser.min.js"></script>
+	<script src="../assets/js/breakpoints.min.js"></script>
+	<script src="../assets/js/util.js"></script>
+	<script src="../assets/js/main.js"></script>
 	<script type="text/javascript">
 		$("#buttonCategory").hideOnClick();
 		/*
@@ -115,12 +121,6 @@
 		});
 		*/
 	</script>
-	<script src="../assets/js/jquery.min.js"></script>
-	<script src="../assets/js/jquery.scrolly.min.js"></script>
-	<script src="../assets/js/browser.min.js"></script>
-	<script src="../assets/js/breakpoints.min.js"></script>
-	<script src="../assets/js/util.js"></script>
-	<script src="../assets/js/main.js"></script>
 
 </body>
 </html>
