b298fc31c6fca723b25b153a399369e79fe0deef
sergiosanchez71 <sergiosanalv@gmail.com>
2022-06-08 21:08:05 +0200

    Update actions.php

--- admin/controller/actions.php
+++ admin/controller/actions.php
@@ -34,10 +34,10 @@ switch ($action) {
 		$questions = $_REQUEST['questions'];
 		$category = $_REQUEST['category'];
 		$createCuestionary_sql = "INSERT INTO questionnaires('name', 'questions', 'category') VALUES ('$name','$questions','$category')";
-		$createCuestionary = $mysqli->exec($categories_sql)
-		if ($createCuestionary = $mysqli->exec($categories_sql)){
-			echo "Cuestionario creado correctamente";
-		}
+		$createCuestionary = $mysqli->exec($categories_sql);
+		
+		echo "Cuestionario creado correctamente";
+		
 
 
 
