714f81476d7ba2a958a867a0bf476aa9faa64cf2
sergiosanchez71 <sergiosanalv@gmail.com>
2022-06-13 19:37:57 +0200

    Update index.php

--- admin/index.php
+++ admin/index.php
@@ -99,6 +99,7 @@
 
 						if ($questionnaires) {
 							foreach ($questionnaires as $questionnaire) {
+								echo $questionnaire['id'];
 								$id = $questionnaire['id'];
 								echo "<option value=$id>";
 								echo $questionnaire['name'];
