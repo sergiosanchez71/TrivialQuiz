5ade614d417fba0638c320c4c6f546375ed34b8b
sergiosanchez71 <sergiosanalv@gmail.com>
2022-06-13 19:22:53 +0200

    Update index.php

--- admin/index.php
+++ admin/index.php
@@ -234,7 +234,7 @@
             	}
             },
             error: function (xhr, status) {
-                            console.log("Error en el logueo"); //El mensaje que se muestra en el caso de que haya un error en la consulta
+                            console.log("Error al mostrar la categoría"); //El mensaje que se muestra en el caso de que haya un error en la consulta
                         },
                         type: "POST",
                         dataType: "text"
