09e1f99c197cb80a94a34710b318118719768d0a
sergiosanchez71 <sergiosanalv@gmail.com>
2022-06-13 20:51:55 +0200

    Update actions.php

--- admin/controller/actions.php
+++ admin/controller/actions.php
@@ -51,8 +51,11 @@ switch ($action) {
 		} */
 
 		$result = mysqli_query($mysqli, "SELECT C.id, C.name FROM questionnaires AS Q,categories AS C WHERE Q.id='$id' and Q.category=C.id");   
-		while($row = mysqli_fetch_assoc($result))
-		    $test[] = $row; 
+		while($row = mysqli_fetch_assoc($result)){
+			if ($row) {
+		    	$test[] = $row; 
+			}
+		}
 		echo json_encode($test);
 
 
