e50a0b2d044628220d0d1f34c7ed665775293c52
sergiosanchez71 <sergiosanalv@gmail.com>
2022-06-13 20:41:49 +0200

    Update actions.php

--- admin/controller/actions.php
+++ admin/controller/actions.php
@@ -43,11 +43,11 @@ switch ($action) {
 	break;
 	case 'searchCategoryQuestionnaire':
 		$id = $_REQUEST['id'];
-		$category_sql = "SELECT category FROM questionnaires";
+		$category_sql = $mysqli->query("SELECT category FROM questionnaires WHERE id='$id'");
 		$category = $mysqli->query($category_sql);
 
 		if ($category) {
-			echo json_encode($category);
+			echo json_encode($category_sql);
 		} 
 
 
