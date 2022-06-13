e73fd1cfba1b527dcc8e8e1acf8efd517a9228f6
sergiosanchez71 <sergiosanalv@gmail.com>
2022-06-13 19:56:18 +0200

    Update actions.php

--- admin/controller/actions.php
+++ admin/controller/actions.php
@@ -43,12 +43,11 @@ switch ($action) {
 	break;
 	case 'searchCategoryQuestionnaire':
 		$id = $_REQUEST['id'];
-		echo $id;
-		/*$query = "SELECT category from questionnaires where id=$id";
+		$query = "SELECT category from questionnaires where id=$id";
 		$category = $mysqli->query($query);
 		if ($category) {
 			echo $category;
-		}*/
+		}
 
 
 	default:
