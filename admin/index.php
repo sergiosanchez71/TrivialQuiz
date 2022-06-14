9f5c0f9e6b79c4b9a75e53c5b3e9a4bf4c1bf1e7
sergiosanchez71 <sergiosanalv@gmail.com>
2022-06-14 19:39:18 +0200

    Update index.php

--- admin/index.php
+++ admin/index.php
@@ -187,10 +187,22 @@
 						?>
 					</select> </p>
 					<p>Respuestas:</p>
-					<p><input type="text" id="nameCrearPreguntasResp1"><input type="radio" name="correcta" value="1" checked></p>
-					<p><input type="text" id="nameCrearPreguntasResp2"><input type="radio" name="correcta" value="2"></p>
-					<p><input type="text" id="nameCrearPreguntasResp3"><input type="radio" name="correcta" value="3"></p>
-					<p><input type="text" id="nameCrearPreguntasResp4"><input type="radio" name="correcta" value="4"></p>
+					<p>
+						<input type="radio" name="correcta" value="1" checked>
+						<input type="text" id="nameCrearPreguntasResp1">
+					</p>
+					<p>
+						<input type="radio" name="correcta" value="2">
+						<input type="text" id="nameCrearPreguntasResp2">
+					</p>
+					<p>
+						<input type="radio" name="correcta" value="3">
+						<input type="text" id="nameCrearPreguntasResp3">
+					</p>
+					<p>
+						<input type="radio" name="correcta" value="4">
+						<input type="text" id="nameCrearPreguntasResp4">
+					</p>
 					<a id="createQuestion" class="button primary icon solid fa-comments-question">Crear pregunta</a>
 					<a id="buttonBackCreateForm" class="button primary icon solid fa-comments-question">Volver</a>
 				</form>
