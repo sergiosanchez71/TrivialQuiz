153580bf382fea25e42a05757917a13b6f031267
sergiosanchez71 <sergiosanalv@gmail.com>
2022-06-14 19:31:50 +0200

    Update index.php

--- admin/index.php
+++ admin/index.php
@@ -175,9 +175,11 @@
 						if ($categories) {
 							foreach ($categories as $category) {
 								$id = $category['id'];
-								echo "<option value=$id>";
-								echo $category['name'];
-								echo "</option>";
+								if($id != 0){
+									echo "<option value=$id>";
+									echo $category['name'];
+									echo "</option>";
+								}
 							}
 						}
 
@@ -236,12 +238,12 @@
 			$("#gestionarCuestionariosForm").css("display","none");
 			$("#modificarCuestionariosForm").css("display","block");
 			searchQuestionnaire($("#nameModificarCuestionariosForm").val());
-		
+			
 		});
 
 		$("#nameModificarCuestionariosForm").change(function(){
 			searchQuestionnaire($("#nameModificarCuestionariosForm").val());
-		
+			
 		});
 
 		$("#buttonBackModifyForm").click(function(){
@@ -291,27 +293,27 @@
 			var parametros = {
 				"action": "createCuestionary",
 				"name": name,
-                "category": category, 
-                "question": question 
-            };
+				"category": category, 
+				"question": question 
+			};
 
-            $.ajax({
-            	url: "controller/actions.php",
-            	data: parametros,
-            success: function (respuesta) { 
-            	if (respuesta) {
-            		console.log(respuesta);
-            	}
-            },
-            error: function (xhr, status) {
+			$.ajax({
+				url: "controller/actions.php",
+				data: parametros,
+				success: function (respuesta) { 
+					if (respuesta) {
+						console.log(respuesta);
+					}
+				},
+				error: function (xhr, status) {
                             console.log("Error en el logueo"); //El mensaje que se muestra en el caso de que haya un error en la consulta
                         },
                         type: "POST",
                         dataType: "text"
                     });
-        }
+		}
 
-        function modifyCuestionary(id, name, category, question){
+		function modifyCuestionary(id, name, category, question){
 			if (question < 5) {
 				question = 5;
 			} else if (question > 100){
@@ -322,78 +324,78 @@
 				"action": "modifyCuestionary",
 				"id": id,
 				"name": name,
-                "category": category, 
-                "question": question 
-            };
+				"category": category, 
+				"question": question 
+			};
 
-            $.ajax({
-            	url: "controller/actions.php",
-            	data: parametros,
-            success: function (respuesta) { 
-            	if (respuesta) {
-            		console.log(respuesta);
-            	}
-            },
-            error: function (xhr, status) {
+			$.ajax({
+				url: "controller/actions.php",
+				data: parametros,
+				success: function (respuesta) { 
+					if (respuesta) {
+						console.log(respuesta);
+					}
+				},
+				error: function (xhr, status) {
                             console.log("Error al modificar el cuestionario"); //El mensaje que se muestra en el caso de que haya un error en la consulta
                         },
                         type: "POST",
                         dataType: "text"
                     });
-        }
+		}
 
-        function deleteCuestionary(id){
+		function deleteCuestionary(id){
 
 			var parametros = {
 				"action": "deleteCuestionary",
 				"id": id
-            };
+			};
 
-            $.ajax({
-            	url: "controller/actions.php",
-            	data: parametros,
-            success: function (respuesta) { 
-            	if (respuesta) {
-            		console.log(respuesta);
-            	}
-            },
-            error: function (xhr, status) {
+			$.ajax({
+				url: "controller/actions.php",
+				data: parametros,
+				success: function (respuesta) { 
+					if (respuesta) {
+						console.log(respuesta);
+					}
+				},
+				error: function (xhr, status) {
                             console.log("Error al borrar el cuestionario"); //El mensaje que se muestra en el caso de que haya un error en la consulta
                         },
                         type: "POST",
                         dataType: "text"
                     });
-        }
+		}
 
-        function searchQuestionnaire(id){
-        	var parametros = {
-        		"action": "searchQuestionnaire",
-        		"id": id
-        	};
+		function searchQuestionnaire(id){
+			var parametros = {
+				"action": "searchQuestionnaire",
+				"id": id
+			};
 
-        	$.ajax({
-        		url: "controller/actions.php",
-        		data: parametros,
-        		success: function (respuesta) { 
-        			console.log(respuesta);
-        			if (respuesta) {
-        				var resp = JSON.parse(respuesta);
-        				$("#newNameModificarCuestionariosForm").val(resp[0].questionnaire);
-        				$("#modificarCuestionarioCategoriaActual").html(resp[0].name);
-        				$("#questionModificarCuestionariosForm").val(resp[0].question);
-        			} 
-        		},
-        		error: function (xhr, status) {
+			$.ajax({
+				url: "controller/actions.php",
+				data: parametros,
+				success: function (respuesta) { 
+					console.log(respuesta);
+					if (respuesta) {
+						var resp = JSON.parse(respuesta);
+						$("#newNameModificarCuestionariosForm").val(resp[0].questionnaire);
+						$("#modificarCuestionarioCategoriaActual").html(resp[0].name);
+						$("#questionModificarCuestionariosForm").val(resp[0].question);
+					} 
+				},
+				error: function (xhr, status) {
                             console.log("Error al mostrar el cuestionario: "+xhr+status); //El mensaje que se muestra en el caso de que haya un error en la consulta
                         },
                         type: "POST",
                         dataType: "text"
                     });
-        }
+		}
 
 
 
-    </script>
+	</script>
 
 </body>
 </html>
