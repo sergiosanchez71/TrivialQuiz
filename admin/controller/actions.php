74123cbbc8c5da590cb0cae7f2f2bd4f8e629f7a
sergiosanchez71 <sergiosanalv@gmail.com>
2022-06-14 18:24:35 +0200

    Update actions.php

--- admin/controller/actions.php
+++ admin/controller/actions.php
@@ -8,48 +8,48 @@ $action = $_REQUEST['action'];
 
 switch ($action) {
 	case 'enter':
-		$username = $_REQUEST['username'];
-		$password = $_REQUEST['password'];
+	$username = $_REQUEST['username'];
+	$password = $_REQUEST['password'];
 
-		$users_sql = "SELECT * FROM users";
-		$users = $mysqli->query($users_sql);
+	$users_sql = "SELECT * FROM users";
+	$users = $mysqli->query($users_sql);
 
-		if ($users) {
-			foreach ($users as $user) {
-				if ($username == $user['name']){
-					if (Password::verify($password, $user['password'])) {
-						echo 'login true';
-						session_start();
-						$_SESSION['admin'] = true;
-					} else {
-						echo "Contraseña incorrecta!";
-					}
+	if ($users) {
+		foreach ($users as $user) {
+			if ($username == $user['name']){
+				if (Password::verify($password, $user['password'])) {
+					echo 'login true';
+					session_start();
+					$_SESSION['admin'] = true;
+				} else {
+					echo "Contraseña incorrecta!";
+				}
 
-				} 
-			}
+			} 
 		}
+	}
 	break;
 	case 'createCuestionary':
-		$name = $_REQUEST['name'];
-		$questions = $_REQUEST['questions'];
-		$category = $_REQUEST['category'];
-		$sql = "INSERT INTO questionnaires VALUES (null, '$name','$questions','$category')";
+	$name = $_REQUEST['name'];
+	$questions = $_REQUEST['questions'];
+	$category = $_REQUEST['category'];
+	$sql = "INSERT INTO questionnaires VALUES (null, '$name','$questions','$category')";
 
-		if (mysqli_query($mysqli, $sql)) {
-		     echo "Cuestionario creado correctamente";
-		}
-		
+	if (mysqli_query($mysqli, $sql)) {
+		echo "Cuestionario creado correctamente";
+	}
+	
 	
 	break;
 	case 'searchQuestionnaire':
-		$id = $_REQUEST['id'];
-		$result = mysqli_query($mysqli, "SELECT C.id, C.name, Q.questions FROM questionnaires AS Q,categories AS C WHERE Q.id='$id' and Q.category=C.id");   
-		while($row = mysqli_fetch_assoc($result)){
-			if ($row) {
-		    	$test[] = $row; 
-				echo json_encode($test);
-			} 
-		}
+	$id = $_REQUEST['id'];
+	$result = mysqli_query($mysqli, "SELECT C.id, C.name, Q.questions FROM questionnaires AS Q,categories AS C WHERE Q.id='$id' and Q.category=C.id");   
+	while($row = mysqli_fetch_assoc($result)){
+		if ($row) {
+			$test[] = $row; 
+		} 
+	}
+	echo json_encode($test);
 
 
 	break;
