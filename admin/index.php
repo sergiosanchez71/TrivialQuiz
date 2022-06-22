ee52b70dbf065b3005eb91f1423ac6a2e8bbee0c
sergiosanchez71 <sergiosanalv@gmail.com>
2022-06-22 21:41:43 +0200

    <git-autocommit>

--- admin/index.php
+++ admin/index.php
@@ -504,7 +504,7 @@
 				url: "controller/actions.php",
 				data: parametros,
 				success: function (respuesta) { 
-					console.log(respuesta); //
+					console.log(respuesta);
 					if (respuesta) {
 						var resp = JSON.parse(respuesta);
 						console.log(resp.replies);
