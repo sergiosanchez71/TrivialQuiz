ead2d1a6ba966a8e2f3712b379332dd608c76ef3
sergiosanchez71 <sergiosanalv@gmail.com>
2022-06-22 21:06:35 +0200

    <git-autocommit>

--- admin/index.php
+++ admin/index.php
@@ -505,7 +505,7 @@
 				data: parametros,
 				success: function (respuesta) { 
 					if (respuesta) {
-						console.log(respuesta[0]);
+						console.log(respuesta[1]);
 						/*for (var i = respuesta.length - 1; i >= 0; i--) {
 							console.log(respuesta[i]);
 						}*/
