1c091cb87b25917108b23e97da8dc5244a007a41
sergiosanchez71 <sergiosanalv@gmail.com>
2022-06-22 20:47:09 +0200

    <git-autocommit>

--- admin/index.php
+++ admin/index.php
@@ -217,6 +217,7 @@
 						if ($questions) {
 							foreach ($questions as $question) {
 								$id = $question['id'];
+								console.log($id);
 								if($id != 0){
 									echo "<option value=$id>";
 									echo $question['name'];
