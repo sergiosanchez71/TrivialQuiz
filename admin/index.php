e57e2b8165ac19a3669d2437fab8e12c0f6a9423
sergiosanchez71 <sergiosanalv@gmail.com>
2022-06-22 20:42:58 +0200

    <git-autocommit>

--- admin/index.php
+++ admin/index.php
@@ -427,7 +427,7 @@
 				data: parametros,
 				success: function (respuesta) { 
 					if (respuesta) {
-						console.log(respuesta[0]); //Mostramos id respuesta
+						console.log(respuesta); //Mostramos id respuesta
 					}
 				},
 				error: function (xhr, status) {
