b095fd2cb038bcba17a5896da41d863deeb9da13
sergiosanchez71 <sergiosanalv@gmail.com>
2022-06-22 20:54:03 +0200

    <git-autocommit>

--- admin/index.php
+++ admin/index.php
@@ -373,7 +373,7 @@
 		});
 
 		$("#questionModificarPreguntasForm").change(function(){
-			searchRepliesFromQuestion($("questionModificarPreguntasForm").val()); //Buscar respuestas dado el ID de la pregunta a modificar
+			//searchRepliesFromQuestion($("questionModificarPreguntasForm").val()); //Buscar respuestas dado el ID de la pregunta a modificar
 			console.log($("questionModificarPreguntasForm").val()); //mostrar valor
 		});
 
