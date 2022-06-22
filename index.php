7dd128bee3529e8428f3cb6ae9c49c5fe39d2531
sergiosanchez71 <sergiosanalv@gmail.com>
2022-06-22 20:16:19 +0200

    Prueba

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 
 		include("admin/controller/conexion.php");
 
-		//$sql = "INSERT INTO questionnaires('1','Cuestionario 2','10',NULL)";
+		//$sql = "INSERT INTO questionnaires('1','Cuestionario 2','10',NULL)"; a
 		$questionnaires_sql = "SELECT * FROM questionnaires";
 		$questionnaires = $mysqli->query($questionnaires_sql);
 
