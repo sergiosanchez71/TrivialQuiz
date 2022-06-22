038ccddf31826a1caf6ebe521c70b536ec01a845
sergiosanchez71 <sergiosanalv@gmail.com>
2022-06-22 21:27:48 +0200

    <git-autocommit>

--- admin/index.php
+++ admin/index.php
@@ -507,7 +507,7 @@
 					if (respuesta) {
 						var resp = JSON.parse(respuesta);
 						for (var i = 1; i <= resp.length; i++) {
-							$("#nameModifPreguntasResp"+i).val(resp[i-1]);
+							$("#nameModifPreguntasResp"+i).val(resp[i-1]); //
 						}
 					} 
 				},
