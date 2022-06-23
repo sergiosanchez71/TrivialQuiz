bb7b5fb0d760cf3564eab65d7d39c1d3c8a952d1
sergiosanchez71 <sergiosanalv@gmail.com>
2022-06-23 20:18:15 +0200

    <git-autocommit>

--- admin/index.php
+++ admin/index.php
@@ -706,7 +706,7 @@
 					}
 				},
 				error: function (xhr, status) {
-                            console.log("Error al modificar la pregunta"); //El mensaje que se muestra en el caso de que haya un error en la consulta
+                            console.log("Error al modificar la categoría"); //El mensaje que se muestra en el caso de que haya un error en la consulta
                         },
                         type: "POST",
                         dataType: "text"
