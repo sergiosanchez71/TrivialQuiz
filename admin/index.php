cd12c2e5380f59ac163f57d0aef9fb130ed27469
sergiosanchez71 <sergiosanalv@gmail.com>
2022-06-23 20:20:00 +0200

    <git-autocommit>

--- admin/index.php
+++ admin/index.php
@@ -522,7 +522,7 @@
 			searchInfoFromCuestionary($("#nameModificarCategoriasForm").val());
 		});
 
-		$("#modifyQuestion").click(function(){
+		$("#modifyCategory").click(function(){
 			modifyCategory($("#nameModificarCategoriasForm").val(), $("#newNameModificarCategoriasForm").val());
 		});
 
