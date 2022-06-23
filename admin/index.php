f13ce1b80d09157dd62cefa7f576dc7a185676ca
sergiosanchez71 <sergiosanalv@gmail.com>
2022-06-23 21:07:20 +0200

    <git-autocommit>

--- admin/index.php
+++ admin/index.php
@@ -130,7 +130,7 @@
 
 						?>
 					</select> </p>
-					<p>Preguntas <input type="number" id="questionModificarCuestionariosForm" min="5" max="100"></p>
+					<p>Preguntas <input type="number" id="questionCreateCuestionariosForm" min="5" max="100"></p>
 					<a id="modifyCuestionary" class="button primary icon solid fa-comments-question">Modificar cuestionario</a>
 					<a id="buttonBackModifyForm" class="button primary icon solid fa-comments-question">Volver</a>
 				</form>
