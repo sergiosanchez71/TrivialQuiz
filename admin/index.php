fb485abd67f7cf6ef72edf1f230c84276c831173
sergiosanchez71 <sergiosanalv@gmail.com>
2022-06-23 19:58:49 +0200

    <git-autocommit>

--- admin/index.php
+++ admin/index.php
@@ -520,7 +520,7 @@
 
 		$("#buttonDeleteCategory").click(function(){
 			$("#gestionarCategoriasForm").css("display","none");
-			$("#borrarCategoriasForm").css("display","block");//
+			$("#borrarCategoriasForm").css("display","block");
 		});
 
 
