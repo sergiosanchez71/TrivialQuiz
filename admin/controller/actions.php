cfa2ece8fa47252637bbef8392587c16475b1850
sergiosanchez71 <sergiosanalv@gmail.com>
2022-06-27 21:29:34 +0200

    <git-autocommit>

--- admin/controller/actions.php
+++ admin/controller/actions.php
@@ -98,10 +98,10 @@ switch ($action) {
 			$question = array(
 			   	"id" => $row["id"],
 			 	"name" => $row["name"],
-			 	"idReplie" => $row["idReplie"],
-			 	"nameReplie" => $row["nameReplie"],
+			 	//"idReplie" => $row["idReplie"],
+			 	//"nameReplie" => $row["nameReplie"],
 			 	"success" => $row["success"],
-			 	"category" => $row["category"] //
+			 	"category" => $row["category"]
 			);
 
 			array_push($questionsArray, $question);
