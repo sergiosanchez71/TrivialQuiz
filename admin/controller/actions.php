0682e34d3991b42ca29007134dfbef944dbe53b1
sergiosanchez71 <sergiosanalv@gmail.com>
2022-06-27 21:27:25 +0200

    <git-autocommit>

--- admin/controller/actions.php
+++ admin/controller/actions.php
@@ -78,7 +78,7 @@ switch ($action) {
 
 		$id = $_REQUEST['id'];
 
-		$sql = "SELECT Q.id as id, Q.name as name, Q.success as success, Q.category as category FROM questions as Q, replies as R WHERE category=(SELECT category FROM questionnaires WHERE id='$id') and Q.id=R.question";
+		$sql = "SELECT Q.id as id, Q.name as name, Q.success as success, Q.category, R.id, R.name as category FROM questions as Q, replies as R WHERE category=(SELECT category FROM questionnaires WHERE id='$id') and Q.id=R.question";
 
 		$questionsArray = array();
 		$result = mysqli_query($mysqli, $sql);
