db2ef118a7663432dec4505746a89dad80d9141f
sergiosanchez71 <sergiosanalv@gmail.com>
2022-06-27 18:49:02 +0200

    <git-autocommit>

--- index.php
+++ index.php
@@ -78,6 +78,7 @@
 				url: "admin/controller/actions.php",
 				data: parametros,
 				success: function (respuesta) { 
+					console.log(respuesta);
 					if (respuesta) {
 						var resp = JSON.parse(respuesta);
 						$("#playQuestionnaires").val(resp);
