f9a8540d467374cec22c68f1f97571d1ae8c23b1
sergiosanchez71 <sergiosanalv@gmail.com>
2022-06-27 20:22:31 +0200

    <git-autocommit>

--- admin/controller/actions.php
+++ admin/controller/actions.php
@@ -55,8 +55,8 @@ switch ($action) {
 
 	break;
 	case 'searchPlayQuestionnaire':
-		$sql = "SELECT id, name FROM questionnaires WHERE questions=(SELECT SUM(QU.id) FROM questions as QU, questionnaires as Q WHERE Q.category=QU.category)";
-		//$sql = "SELECT id, name FROM questionnaires";
+		//$sql = "SELECT id, name FROM questionnaires WHERE questions=(SELECT SUM(QU.id) FROM questions as QU, questionnaires as Q WHERE Q.category=QU.category)";
+		$sql = "SELECT * FROM questionnaires";
 		//$sql = SELECT SUM(QU.id) FROM questions as QU, questionnaires as Q WHERE Q.category=QU.category;
 
 		$questionnairesArray = array();
