79d5ebcb202ee33712711f3b5e23519735b20e7c
sergiosanchez71 <sergiosanalv@gmail.com>
2022-07-18 19:10:36 +0200

    Update conexion.php

--- admin/controller/conexion.php
+++ admin/controller/conexion.php
@@ -12,7 +12,7 @@ $mysqli = new mysqli($servername, $username, $password, $database);
 
 if ($mysqli->connect_errno) {
     echo "Fallo al conectar a MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
-$ip = gethostbyname('http://sergiosanchez.site');
+$ip = gethostbyname('sergiosanchez.site');
 echo $ip;
 }
 
