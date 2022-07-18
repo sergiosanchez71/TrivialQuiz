1f6822edde2d3e0f25c5b33cfc912299354a11e1
sergiosanchez71 <sergiosanalv@gmail.com>
2022-07-18 19:21:21 +0200

    Update conexion.php

--- admin/controller/conexion.php
+++ admin/controller/conexion.php
@@ -11,7 +11,7 @@ $database = DATABASE;
 $mysqli = new mysqli($servername, $username, $password, $database);
 
 if ($mysqli->connect_errno) {
-    echo "Fallo al conectar a MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+    //echo "Fallo al conectar a MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
 }
 
 
